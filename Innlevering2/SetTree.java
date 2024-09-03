bc184c433d6d13eba6b237c53a5bbbb2999d19f3
Thomas Heim <wearedreamin@gmail.com>
2024-09-03 14:24:01 +0200

    update tree🌳

--- Innlevering2/SetTree.java
+++ Innlevering2/SetTree.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+// Oppretter en trestruktur for sett.
 class TreeNode {
     int x;
     TreeNode left, right;
@@ -11,7 +12,7 @@ class TreeNode {
         left = right = null;
     }
 }
-
+// Oppretter en rot og størrelse. 
 class SetBST {
     private TreeNode root;
     private int size;
@@ -20,71 +21,83 @@ class SetBST {
         root = null;
         size = 0;
     }
-    
+    // Bruk
     public boolean contains(SetBST set, int x) {
         return contains(set.root, x);
     }
-    
+    // Hjelpemetode for å sjekke om et element er i settet.
     private boolean contains(TreeNode node, int x) {
+        // Basistilfelle. 
         if (node == null) {
             return false;
         }
+        // Hvis vi har funnet elementet.
         if (node.x == x) {
             return true;
         } 
+        // Hvis elementet er mindre enn noden, gå til venstre.
         if (x < node.x) {
             return contains(node.left, x);
         } 
+        // Hvis elementet er større enn noden, gå til høyre.
         return contains(node.right, x);
     }
-    
+    // Bruker den rekursive hjelpemetoden for å sette inn et element i settet.
     public void insert(SetBST set, int x) {
         set.root = insert(set.root, x);
     }
-    
+    // Rekursiv hjelpemetode for å sette inn et element i settet.
     private TreeNode insert(TreeNode node, int x) {
+        // Basistilfelle.
         if (node == null) {
             size++;
             return new TreeNode(x);
         }
+        // Hvis elementet er mindre enn noden, gå til venstre.
         if (x < node.x) {
             node.left = insert(node.left, x);
+            // Hvis elementet er større enn noden, gå til høyre.
         } else if (x > node.x) {
             node.right = insert(node.right, x);
         }
         return node;
     }
-    
+    // Bruker den rekursive hjelpemetoden for å fjerne et element fra settet.
     public void remove(SetBST set, int x) {
         set.root = remove(set.root, x);
     }
-    
+    // Rekursiv hjelpemetode for å fjerne et element fra settet.
     private TreeNode remove(TreeNode node, int x) {
+        // Basistilfelle.
         if (node == null) {
             return node;
         }
+        // Hvis elementet er mindre enn noden, gå til venstre.
         if (x < node.x) {
             node.left = remove(node.left, x);
             return node;
         }
+        // Hvsi elementet er større enn noden, gå til høyre.
         if (x > node.x) {
             node.right = remove(node.right, x);
             return node;
         }
+        // Hvis noden har ett eller ingen barn.
         if (node.left == null) {
             size--;
             return node.right;
         }
+        // Hvis noden har ett eller ingen barn.
         if (node.right == null) {
             size--;
             return node.left;
         }
-        
+        // Hvis noden har to barn.
         node.x = minValue(node.right);
         node.right = remove(node.right, node.x);
         return node;
     }
-    
+    // Hjelpemetode for å finne minste verdi i et tre.
     private int minValue(TreeNode node) {
         int minv = node.x;
         while (node.left != null) {
@@ -93,12 +106,13 @@ class SetBST {
         }
         return minv;
     }
-    
+    // Henter størrelsen på settet.
     public int size(SetBST set) {
         return size;
     }
 }
 
+// Leser inn fil og utfører operasjoner på settet.
 public class SetTree {
 
     public static void main(String[] args) throws IOException {
