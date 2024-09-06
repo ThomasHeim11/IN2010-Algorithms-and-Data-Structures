cc5f2f3755a4870e6b968969aa675df5f449a7ee
Thomas Heim <wearedreamin@gmail.com>
2024-09-06 12:12:28 +0200

    heap🤩

---
+++ Innlevering2/Heap.java
@@ -0,0 +1,12 @@
+//TODO PriorityQueue2
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+
+//TODO 1) PLASSERE TALL I HEAP
+
+
+//TODO 2) Kalle på impletasjonen av algortimen
+
+
+// Todo 3) Skrive ut resultatet i main. 
