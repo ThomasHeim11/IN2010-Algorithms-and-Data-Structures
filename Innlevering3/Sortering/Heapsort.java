a58710c96c6ba6cbab8200d3dee56ea819577a92
Thomas Heim <wearedreamin@gmail.com>
2024-09-17 12:14:00 +0200

    nice

--- Innlevering3/Sortering/Heapsort.java
+++ Innlevering3/Sortering/Heapsort.java
@@ -1,3 +1,10 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 // Værste kjøretid for heapsort O(n*log(n)).
 
 public class Heapsort{
@@ -24,9 +31,10 @@ public class Heapsort{
             heapify(A,i,n);
         }
     }
+
         // Funksjonen for heapify
-        private static void heapify(int[] A, int i, int n){
-            int left = 2*1+1;
+    private static void heapify(int[] A, int i, int n){
+            int left = 2*i+1;
             int right = 2*i+2;
             int max = i;
         
@@ -50,4 +58,36 @@ public class Heapsort{
             A[x] = A[y];
             A[y] = temp;
         }
+
+    public static void main(String[] args) throws IOException{
+    if(args.length != 1){
+        System.out.println("Bruk: java InsertionSort <filnavn>");
+        System.exit(1);
     }
+    String filnavn = args[0];
+    ArrayList<Integer> tall = new ArrayList<>();
+    // Leser tall fra fil. 
+    try(BufferedReader br = new BufferedReader(new FileReader(filnavn))){
+        String line;
+        while((line = br.readLine()) != null){
+            tall.add(Integer.parseInt(line.trim()));
+        }
+    } catch (IOException e){
+        e.printStackTrace();
+        System.exit(1);
+    }
+    // Konverterer ArrayList til int-array. 
+    int[] array = tall.stream().mapToInt(i -> i).toArray();
+    //Sorterer innsetingssortering. 
+    heapsort(array);
+
+    // Skriv ut til fil
+    try(FileWriter fw = new FileWriter(filnavn+ "_insertion.out")){
+        for(int num : array){
+            fw.write(num + "\n");
+        }
+    } catch (IOException e){
+        e.printStackTrace();
+    }
+}
+}
