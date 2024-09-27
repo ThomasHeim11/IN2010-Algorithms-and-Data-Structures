5e989f50450062dd8518a1d56778a3a77f2cf791
Thomas Heim <wearedreamin@gmail.com>
2024-09-27 12:38:02 +0200

    wrong

--- Innlevering4/SixDegreesIMDB.java
+++ Innlevering4/SixDegreesIMDB.java
@@ -1,98 +1,110 @@
 import java.util.*;
 
-// Bruker bredde først søk for å finne den korteste veien mellom skuespillerne. 
-public class SixDegreesIMDB{
-    String moviesFile = "./movies.tsv";
-    String actorsFile = "./actors.tsv";
-    // Laster fiulmvurderinger og lager en graf av skespillere. 
-    Map<String,Double> movieRatings = MovieGraph.loadMovieRatings(moviesFile);
-    Map<String, MovieGraph.ActorNode> actorGraph = MovieGraph.loadActorGraph(actorsFile,movieRatings);
+public class SixDegreesIMDB {
 
-     // Finn og skriv ut korteste vei mellom forskjellige par av skuespillere
-        kortesteSti(actorGraph, "nm2255973", "nm0000460"); // Donald Glover til Jeremy Irons
-        kortesteSti(actorGraph, "nm0424060", "nm8076281"); // Scarlett Johansson til Emma Mackey
-        kortesteSti(actorGraph, "nm4689420", "nm0000365"); // Carrie Coon til Julie Delpy
-        kortesteSti(actorGraph, "nm0000288", "nm2143282"); // Christian Bale til Lupita Nyong'o
-        kortesteSti(actorGraph, "nm0637259", "nm0931324"); // Tuva Novotny til Michael K. Williams
-}
+    public static void main(String[] args) {
+        // Filstier til film- og skuespillerdatafiler 
+        String moviesFile = "./movies.tsv";
+        String actorsFile = "./actors.tsv";
 
+        // Laster filmvurderinger og lager en graf av skuespillere
+        Map<String, Double> movieRatings = MovieGraph.loadMovieRatings(moviesFile);
+        Map<String, MovieGraph.ActorNode> actorGraph = MovieGraph.loadActorGraph(actorsFile, movieRatings);
 
-public static void kortesteSti(Map<String, MovieGraph.ActorNode> actorGraph, String startId, String goalId){
-    // Sjekker om skuespilleren finners i grafen
-    if(!actorGraph.containsKey(startId) || !actorGraph.containsKey(goalId)){
-        System.out.println("En eller begge skuespillere finnes ikke i grafen");
-        return;
+        // Finn og skriv ut korteste vei mellom forskjellige par av skuespillere
+        findShortestPath(actorGraph, "nm2255973", "nm0000460"); // Donald Glover til Jeremy Irons
+        findShortestPath(actorGraph, "nm0424060", "nm8076281"); // Scarlett Johansson til Emma Mackey
+        findShortestPath(actorGraph, "nm4689420", "nm0000365"); // Carrie Coon til Julie Delpy
+        findShortestPath(actorGraph, "nm0000288", "nm2143282"); // Christian Bale til Lupita Nyong'o
+        findShortestPath(actorGraph, "nm0637259", "nm0931324"); // Tuva Novotny til Michael K. Williams
     }
-    // Variabler for skulespilleren vi går fra og til. 
-    MovieGraph.ActorNode startNode = actorGraph.get(startId);
-    MovieGraph.ActorNode goalNode = actorGraph.get(goalId);
 
-    // Data-struktur for bredde-først søk.
-    Map<MovieGraph.ActorNode,Boolean> visited = new HashMap<>();
-    Queue<MovieGraph.ActorNode> queue = new LinkedList<>();
-    Map<MovieGraph.ActorNode, MovieGraph.ActorNode> previous = new HashMap<>();
-    Map<MovieGraph.ActorNode,MovieGraph.Edge> movieEdge = new HashMap<>();
+    public static void findShortestPath(Map<String, MovieGraph.ActorNode> actorGraph, String startId, String goalId) {
+        // Sjekk om start og målskuespiller finnes i grafen
+        if (!actorGraph.containsKey(startId) || !actorGraph.containsKey(goalId)) {
+            System.out.println("En eller begge skuespillere finnes ikke i grafen");
+            return;
+        }
 
-    // Utfører BFS-SØK fra start.
-    BFSVisit(actorGraph,startNode,visited,queue,previous,movieEdge,goalNode);
-    // Sjekker om målskuespilleren er besøkt
-    if(visited.getOrDefault(goalNode, false)){
-        //Skriver ut stie hvis den er funnet
-        printPath(previous,movieEdge,startNode,goalNode);
-    } else{
-        System.out.println("Ingen sti funnet mellom " + startId + " og " + goalId);
-    } 
-}
+        // Initialiserer start- og målskuespillernoder
+        MovieGraph.ActorNode startNode = actorGraph.get(startId);
+        MovieGraph.ActorNode goalNode = actorGraph.get(goalId);
 
-private static void BFSVisit(Map<String, MovieGraph.ActorNode> actorGraph,
+        // Data-strukturer for BFS
+        Map<MovieGraph.ActorNode, Boolean> visited = new HashMap<>();
+        Queue<MovieGraph.ActorNode> queue = new LinkedList<>();
+        Map<MovieGraph.ActorNode, MovieGraph.ActorNode> previous = new HashMap<>();
+        Map<MovieGraph.ActorNode, MovieGraph.Edge> movieEdge = new HashMap<>();
+
+        // Utfør BFS fra start
+        BFSVisit(actorGraph, startNode, visited, queue, previous, movieEdge, goalNode);
+
+        // Sjekk om målskuespilleren er besøkt
+        if (visited.getOrDefault(goalNode, false)) {
+            // Skriv ut sti hvis den er funnet
+            printPath(previous, movieEdge, startNode, goalNode);
+        } else {
+            System.out.println("Ingen sti funnet mellom " + startId + " og " + goalId);
+        }
+    }
+
+    private static void BFSVisit(Map<String, MovieGraph.ActorNode> actorGraph,
                                  MovieGraph.ActorNode s,
                                  Map<MovieGraph.ActorNode, Boolean> visited,
                                  Queue<MovieGraph.ActorNode> queue,
                                  Map<MovieGraph.ActorNode, MovieGraph.ActorNode> previous,
                                  Map<MovieGraph.ActorNode, MovieGraph.Edge> movieEdge,
                                  MovieGraph.ActorNode goalNode) {
-    // Legg til startnode i besøkte noder og køen
-    visited.put(s,true);
-    queue.add(s);
+        // Legg til startnode i besøkte noder og køen
+        visited.put(s, true);
+        queue.add(s);
 
-    // Utfør BFS
-    while(!queue.isEmpty()){
-        MovieGraph.ActorNode current = queue.poll();
-        // Hvis vi når målskuspiller så avslutter vi.
-        if(current.equals(goalNode)){
-            return;
-        }
-        // Går gjennom alle naboebe(kanter)
-        if(!visited.getOrDefault(neighbor, false)){
-            visited.put(neighbor, true);
-            queue.add(neighbor);
-            previous.put(neighbor, current);
-            movieEdge.put(neighbor, edge);
-        }
-    }
-}
+        // Utfør bredde-først søk
+        while (!queue.isEmpty()) {
+            MovieGraph.ActorNode current = queue.poll();
 
-private static void printPath(Map<MovieGraph.ActorNode, MovieGraph.ActorNode> previous, Map<MovieGraph.ActorNode, MovieGraph.Edge> movieEdge, MovieGraph.ActorNode startNode, MovieGraph.ActorNode goalNode){
-    List<MovieGraph.ActorNode> path = new LinkedList<>();
-    List<MovieGraph.Edge> movies = new LinkedList<>();
+            // Hvis vi har nådd målskuespilleren, avslutt
+            if (current.equals(goalNode)) {
+                return;
+            }
 
-    // Rekonstruerer stien ved å følge previous tilbake fra må til start.
-    for(MovieGraph.ActorNode ved = goalNode; ved != null; ved = previous.get(ved)){
-        path.add(ved);
-        if(movieEdge.get(ved) != null){
-            movies.add(movieEdge.get(ved));
+            // Iterer gjennom alle naboene (kanter)
+            for (MovieGraph.Edge edge : current.edges) {
+                MovieGraph.ActorNode neighbor = edge.actor1.equals(current) ? edge.actor2 : edge.actor1;
+                
+                // Hvis naboskuespilleren ikke er besøkt, legg til i besøkt, kø, og oppdater sti
+                if (!visited.getOrDefault(neighbor, false)) {
+                    visited.put(neighbor, true);
+                    queue.add(neighbor);
+                    previous.put(neighbor, current);
+                    movieEdge.put(neighbor, edge);
+                }
+            }
         }
     }
-    //Snur listen for å få korrekt rekkefølge fra start til målskuespilleren
-    Collection.reverse(path);
-    Collection.reverse(movies);
 
-    // Skriv ut stien med skuespillernavn og film det er tilkoblet.
-    System.out.println(path.get(0).name);
-    for(int i = 1; i < path.size(); i++){
-        MovieGraph.Edge edge = movies.get(i-1);
-        MovieGraph.ActorNode actor = path.get(i);
-        System.out.println("===[ " + edge.ttId + " (" + edge.rating + ") ] ===> " + actor.name);
+    private static void printPath(Map<MovieGraph.ActorNode, MovieGraph.ActorNode> previous, Map<MovieGraph.ActorNode, MovieGraph.Edge> movieEdge, MovieGraph.ActorNode startNode, MovieGraph.ActorNode goalNode) {
+        List<MovieGraph.ActorNode> path = new LinkedList<>();
+        List<MovieGraph.Edge> movies = new LinkedList<>();
+
+        // Rekonstruer stien ved å følge 'previous' tilbake fra mål til start
+        for (MovieGraph.ActorNode at = goalNode; at != null; at = previous.get(at)) {
+            path.add(at);
+            if (movieEdge.get(at) != null) {
+                movies.add(movieEdge.get(at));
+            }
+        }
+
+        // Snur listen for å få korrekt rekkefølge fra start til mål
+        Collections.reverse(path);
+        Collections.reverse(movies);
+
+        // Skriv ut stien med skuespillernavn og film det er tilkoblet
+        System.out.println(path.get(0).name);
+        for (int i = 1; i < path.size(); i++) {
+            MovieGraph.Edge edge = movies.get(i - 1);
+            MovieGraph.ActorNode actor = path.get(i);
+            System.out.println("===[ " + edge.ttId + " (" + edge.rating + ") ] ===> " + actor.name);
+        }
     }
-
 }
